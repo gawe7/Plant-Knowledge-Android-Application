e86c0ce0c6af68cdd4a972b9a25dbac43fb6f2f8
gawe7 <game18130@gmail.com>
2024-02-14 03:29:09 +0700

    olo

---
+++ app/src/main/java/com/example/eartheden/Category.kt
@@ -0,0 +1,11 @@
+package com.example.eartheden
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Category : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
