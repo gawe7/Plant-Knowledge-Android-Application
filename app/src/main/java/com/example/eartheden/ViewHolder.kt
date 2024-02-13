536d8c4a0a15edf80336723d9d49a0219b9036a0
gawe7 <game18130@gmail.com>
2024-02-14 04:50:02 +0700

    olo

---
+++ app/src/main/java/com/example/eartheden/ViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.eartheden
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewHolder (View: View) : RecyclerView.ViewHolder(View){
+    var textTitleItem: TextView = View.findViewById(R.id.textTitleItem)
+    var imageView: ImageView = View.findViewById(R.id.imageView)
+    var cardView: CardView = View.findViewById(R.id.cardView)
+}
