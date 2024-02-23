f78f02c7a9da93fd7965f2b1b7a1802a7d908573
gawe7 <game18130@gmail.com>
2024-02-23 19:22:02 +0700

    olo

--- app/src/main/java/com/example/eartheden/Category.kt
+++ app/src/main/java/com/example/eartheden/Category.kt
@@ -3,6 +3,7 @@ package com.example.eartheden
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,13 +17,14 @@ import com.google.firebase.database.FirebaseDatabase
 
 class Category : AppCompatActivity() {
     private lateinit var binding: ActivityCategoryBinding
-    private lateinit var CateBack: ImageButton
+
     private lateinit var recyclerViewCate: RecyclerView
     private lateinit var mAuth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private lateinit var databaseReferenceCactus: DatabaseReference
     private lateinit var responseCate: MutableList<CateModel>
     private var cateAdapter: CateAdapter? = null
+    private var category_backbtn: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,22 +34,29 @@ class Category : AppCompatActivity() {
 
         init()
 
-        CateBack.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+
 
         // Firebase
         mAuth = FirebaseAuth.getInstance()
-        database = FirebaseDatabase.getInstance()
 
-        recyclerViewCate.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        database = FirebaseDatabase.getInstance("https://eartheden-9818d-default-rtdb.asia-southeast1.firebasedatabase.app")
+
+
+        recyclerViewCate.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         databaseReferenceCactus = database.getReference("cactus")
         responseCate = mutableListOf()
         cateAdapter = CateAdapter(responseCate as ArrayList<CateModel>)
         recyclerViewCate.adapter = cateAdapter
         onBindingFirebase()
+
+
+
+        category_backbtn?.setOnClickListener { onBackPressed() }
     }
+
+
+
 
     private fun onBindingFirebase() {
         databaseReferenceCactus.addChildEventListener(object : ChildEventListener {
@@ -66,8 +75,9 @@ class Category : AppCompatActivity() {
         })
     }
 
-    private fun init() {
-        CateBack = binding.categoryBackbtn ?: throw NullPointerException("Category Back Button is null")
-        recyclerViewCate = binding.recyclerViewCate ?: throw NullPointerException("Recycler View is null")
+    fun init() {
+        category_backbtn = findViewById(R.id.category_backbtn)
+        recyclerViewCate = findViewById(R.id.recyclerViewCate)
+
     }
 }
--- app/src/main/java/com/example/eartheden/ViewHolder.kt
+++ app/src/main/java/com/example/eartheden/ViewHolder.kt
@@ -9,5 +9,5 @@ import androidx.recyclerview.widget.RecyclerView
 class ViewHolder (View: View) : RecyclerView.ViewHolder(View){
     var textTitleItem: TextView = View.findViewById(R.id.textTitleItem)
     var imageView: ImageView = View.findViewById(R.id.imageView)
-    var cardView: CardView = View.findViewById(R.id.cardView)
+
 }
